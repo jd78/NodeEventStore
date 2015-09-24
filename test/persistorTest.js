48e25c0ed06eaf8b2a486c651648d1a4c483c52f
Gennaro Del Sorbo <gennarodelsorbo@icloud.com>
2015-09-25 00:48:12 +0200

    snapshot completed

--- test/persistorTest.js
+++ test/persistorTest.js
@@ -4,8 +4,12 @@ require("should")
 const aggregate = require("./aggregate/demo")
 const Persistor = require("../lib/persistor")
 const inMemoryAdapter = require("../lib/in-memory-persistence-adapter")
+const sinon = require("sinon")
 
 describe('Save Aggregate', function () {
+
+    beforeEach(() => inMemoryAdapter.clean())
+
     it('Create aggregate', function () {
         let a = aggregate.create(1)
         a.initialize('name')
@@ -17,15 +21,15 @@ describe('Save Aggregate', function () {
             a.uncommittedEvents.length.should.equal(0)
         })
     })
-    
+
     it('Create aggregate with snapshot', function () {
         let a = aggregate.create(1, 2)
         a.initialize('name')
         a.updateName('jd')
-        
+
         a.uncommittedEvents.length.should.equal(2);
         a.uncommittedSnapshots.length.should.equal(1);
-        
+
         let persistor = new Persistor(inMemoryAdapter)
 
         return persistor.save(a).then(() => {
@@ -45,6 +49,36 @@ describe('Save Aggregate', function () {
             return persistor.read(1).then(aggregate => {
                 aggregate.name.should.equal('jd')
                 aggregate.uncommittedEvents.length.should.equal(0)
+            })
+        })
+    })
+
+    it('get aggregate with snapshot', function () {
+        let a = aggregate.create(1, 2)
+        a.initialize('name')
+        a.updateName('name2')
+        a.updateName('name3')
+        a.updateName('name4')
+        a.updateName('final')
+
+        let persistor = new Persistor(inMemoryAdapter, aggregate)
+        let readSnapshotSpy = sinon.spy(inMemoryAdapter, "readSnapshot")
+        var readEventsSpy = sinon.spy(inMemoryAdapter, "readEvents")
+
+        return persistor.save(a).then(() => {
+            return persistor.read(1).then(aggregate => {
+                aggregate.name.should.equal('final')
+                aggregate.uncommittedEvents.length.should.equal(0)
+                aggregate.uncommittedSnapshots.length.should.equal(0)
+                return readSnapshotSpy.returnValues[0].then(values => {
+                    values.version.should.equal(4)
+                    values.payload.should.equal('{"name":"name4"}')
+                    return readEventsSpy.returnValues[0].then(events => {
+                        events.length.should.equal(1);
+                        events[0].version.should.equal(5)
+                        events[0].payload.should.equal('{"name":"final","id":1,"version":5}')
+                    })
+                })
             })
         })
     })
