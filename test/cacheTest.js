a7325dfcb8aeb1fbf727a5bb7755c2d4f5043a26
Gennaro Del Sorbo <gennarodelsorbo@icloud.com>
2015-09-25 17:56:35 +0200

    obj clone

--- test/cacheTest.js
+++ test/cacheTest.js
@@ -10,4 +10,14 @@ describe("Cache", () => {
 			result.should.equal("test")
 		})
 	})
+	
+	it("clone", () => {
+		let cache = require("../lib/cache")
+		let obj = { desc: "test" }
+		cache.setKey("1", obj)
+		obj.desc = "test2"
+		return cache.getKey("1").then(result => {
+			result.desc.should.equal("test")
+		})
+	})
 })
