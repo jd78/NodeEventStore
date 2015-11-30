8110383bb2119254b7fe7e60a3b594512411338d
Gennaro Del Sorbo <gennaro.delsorbo@gmail.com>
2015-11-30 18:37:41 +0000

    demo encapsulation fixed

--- demo/event-store-demo.js
+++ demo/event-store-demo.js
@@ -15,27 +15,28 @@ const EventStore = NodeEventStore.initialize({
 })
 
 const repository =  new EventStore.Repository(UserInfoAggregate)
-let userInfoAggregate = UserInfoAggregate.create(1)
+
+let userInfoAggregate = new UserInfoAggregate(1);
 userInfoAggregate.initialize("Gennaro", "Del Sorbo", "Main Street", "09762847")
 
 repository.save(userInfoAggregate).then(() => {
-	userInfoAggregate.updateMobile("333");
-	userInfoAggregate.updateMobile("334");
-	userInfoAggregate.updateMobile("335");
-	userInfoAggregate.updateAddress("12, Main St.")
-	userInfoAggregate.updateAddress("15, Main St.")
-	repository.save(userInfoAggregate).then(() => {
-		console.log("all saved")
+ 	userInfoAggregate.updateMobile("333");
+ 	userInfoAggregate.updateMobile("334");
+ 	userInfoAggregate.updateMobile("335");
+ 	userInfoAggregate.updateAddress("12, Main St.")
+ 	userInfoAggregate.updateAddress("15, Main St.")
+ 	repository.save(userInfoAggregate).then(() => {
+ 		console.log("all saved")
 		
-		console.log("try a read")
+ 		console.log("try a read")
 		
-		repository.read(1).then(userInfo => {
-			console.log(userInfo.Mobile)
-			console.log(userInfo.Address)
-		}).catch(e => {
-			console.log(e)
-		});
-	})
-}).catch(err => {
-	console.log(err)
+ 		repository.read(1).then(userInfo => {
+ 			console.log(userInfo.Mobile)
+ 			console.log(userInfo.Address)
+ 		}).catch(e => {
+ 			console.log(e)
+ 		});
+ 	})
+ }).catch(err => {
+ 	console.log(err)
 })
--- demo/userInfoAggregate.js
+++ demo/userInfoAggregate.js
@@ -6,73 +6,72 @@ const AddressUpdated = require("./dto/addressUpdated")
 const MobileUpdated = require("./dto/mobileUpdated")
 const clone = require("clone")
 
-function UserInfoObj() {
-    this.name
-    this.surname
-    this.address
-    this.mobile
-}
+function UserInfo(id) {
 
-let _userInfo
-
-class UserInfo extends NodeEventStore.Aggregate {
-
-    constructor(id) {
-        super(id)
-        _userInfo = new UserInfoObj()
-    }
-
-    static create(id) {
-        return new UserInfo(id)
-    }
-
-    snapshot() {
-        return clone(_userInfo)
-    }
-
-    applySnapshot(payload) {
-        _userInfo = payload
+    function UserInfoObj() {
+        this.name
+        this.surname
+        this.address
+        this.mobile
     }
     
-    //Queries
-    get Mobile() {
-        return _userInfo.mobile
-    }
-
-    get Address() {
-        return _userInfo.address
+    let _userInfo
+    
+    class UserInfo extends NodeEventStore.Aggregate {
+    
+        constructor(id) {
+            super(id)
+            _userInfo = new UserInfoObj()
+        }
+    
+        snapshot() {
+            return clone(_userInfo)
+        }
+    
+        applySnapshot(payload) {
+            _userInfo = payload
+        }
+        
+        //Queries
+        get Mobile() {
+            return _userInfo.mobile
+        }
+    
+        get Address() {
+            return _userInfo.address
+        }
+        
+        //Mutators
+        initialize(name, surname, address, mobile) {
+            super.raiseEvent(new UserInfoCreated(name, surname, address, mobile))
+        }
+    
+        updateAddress(address) {
+            super.raiseEvent(new AddressUpdated(address))
+        }
+    
+        updateMobile(mobile, hookFn) {
+            super.raiseEvent(new MobileUpdated(mobile), hookFn)
+        }
+        
+        //Apply
+        UserInfoCreated(payload) {
+            _userInfo.name = payload.name
+            _userInfo.surname = payload.surname
+            _userInfo.address = payload.address
+            _userInfo.mobile = payload.mobile
+        }
+    
+        AddressUpdated(payload) {
+            _userInfo.address = payload.address
+        }
+    
+        MobileUpdated(payload) {
+            _userInfo.mobile = payload.mobile
+        }
     }
     
-    //Mutators
-    initialize(name, surname, address, mobile) {
-        super.raiseEvent(new UserInfoCreated(name, surname, address, mobile))
-    }
-
-    updateAddress(address) {
-        super.raiseEvent(new AddressUpdated(address))
-    }
-
-    updateMobile(mobile, hookFn) {
-        super.raiseEvent(new MobileUpdated(mobile), hookFn)
-    }
-    
-    //Apply
-    UserInfoCreated(payload) {
-        _userInfo.name = payload.name
-        _userInfo.surname = payload.surname
-        _userInfo.address = payload.address
-        _userInfo.mobile = payload.mobile
-    }
-
-    AddressUpdated(payload) {
-        _userInfo.address = payload.address
-    }
-
-    MobileUpdated(payload) {
-        _userInfo.mobile = payload.mobile
-    }
+    return new UserInfo(id);
 }
 
-module.exports = {
-    create: UserInfo.create
-}
+module.exports = UserInfo
