f725b334ec0b4ebcb96bd5cdae74bad10164eee2
Jordan Morris <jordan.morris@xero.com>
2016-11-24 15:35:29 +1300

    dependency injection for serializer
    The tests for the serializer were not sharing the configuration
    dependency with the serializer module, therefore changes to the
    configuration were not being reflected in the serializer. The tests were
    passing, because both configurations being tested had the same output
    anyway.

--- test/serializerTest.js
+++ test/serializerTest.js
@@ -1,30 +1,30 @@
 "use strict"
 
 const configuration = require("../lib/configuration")
-const serializer = require("../lib/serializer")
+const serializer = require("../lib/serializer")({ configuration })
 require("should")
 
 describe("Serializer test", () => {
-	
+
 	it("serializer test", () => {
-		
+
 		configuration.zipPayload = false
-		
+
 		let obj = {
 			title: "test"
 		}
-		
+
 		let serialized = serializer.serialize(obj)
 		let deserialized = serializer.deserialize(serialized)
-		
+
 		deserialized.title.should.equal(obj.title)
-		
+
 		configuration.zipPayload = true
-		
+
 		serialized = serializer.serialize(obj)
 		deserialized = serializer.deserialize(serialized)
-		
+
 		deserialized.title.should.equal(obj.title)
 	})
-	
+
 })
